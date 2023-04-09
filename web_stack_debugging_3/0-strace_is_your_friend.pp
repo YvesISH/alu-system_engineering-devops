65255fb5b3cc69cee455c418e2ae0844228a5d0f
YvesISH <y.ishimwe2@alustudent.com>
2023-04-09 16:35:49 -0700

    Strace is your Friend

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix Appache 500, automate with puppet
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
